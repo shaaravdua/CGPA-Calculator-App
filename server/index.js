02907a5436048a472042d8dbc2d5c14949f8a010
shaaravdua <shaaravdua@gmail.com>
2026-04-06 21:18:35 +0000

    Updated CORS

--- server/index.js
+++ server/index.js
@@ -13,7 +13,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173', 
+    'https://cgpa-calculator-app-alpha.vercel.app' 
+  ],
   credentials: true,
 }));
 app.use(express.json());
